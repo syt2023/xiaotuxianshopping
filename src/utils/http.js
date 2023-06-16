f792c6542f060df86fc01930c8877ca748c07bac
sunyt <2312591236@qq.com>
2023-06-16 15:51:01 +0800

    6.16

---
+++ src/utils/http.js
@@ -0,0 +1,85 @@
+// // axios基础的封装
+// import axios from 'axios'
+// import { ElMessage } from 'element-plus'
+// import { useUserStore } from '@/stores/userStore.js'
+
+// const httpInstance = axios.create({
+//   baseURL: 'https://www.apifox.cn/apidoc/shared-c05cb8d7-e591-4d9c-aff8-11065a0ec1de/api-67132167',
+//   timeout: 5000
+// })
+
+// // 拦截器
+
+// // axios请求拦截器
+// httpInstance.interceptors.request.use(config => {
+//   //1、从pinia获取token数据
+//   const userStore = useUserStore()
+//   //2、按照后端的要求拼接token数据
+//   const token = userStore.userInfo.token
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// }, e => Promise.reject(e))
+
+// // axios响应式拦截器
+// httpInstance.interceptors.response.use(res => res.data, e => {
+
+//   //统一错误提示
+//   ElMessage({
+//     type: 'warning',
+//     message: e.response.data.message
+//   })
+
+//   //401token失效处理
+//   //1、清除本地用户数据
+//   //2、跳转到登录页
+//   // if ( e.response.statis === 401){
+//   //   userStore.clearUserInfo()
+//   //   router.push('/login')
+
+//   // }
+//   return Promise.reject(e)
+// })
+
+
+// export default httpInstance
+
+
+
+
+// axios基础的封装
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/userStore'
+const httpInstance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
+})
+ 
+// 拦截器
+ 
+// axios请求拦截器
+httpInstance.interceptors.request.use(config => {
+  // 1. 从pinia获取token数据
+  const userStore = useUserStore()
+  // 2. 按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, e => Promise.reject(e))
+ 
+// axios响应式拦截器
+httpInstance.interceptors.response.use(res => res.data, e => {
+  // 统一错误提示
+  ElMessage({
+    type: 'warning',
+    message: e.response.data.message
+  })
+  return Promise.reject(e)
+})
+ 
+ 
+export default httpInstance
